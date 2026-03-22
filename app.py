a21773927c27f45899e9ca79c246d88c9162fd47
Akshaykp <ranjugowdaputtur123@gmail.com>
2026-03-22 22:52:45 +0530

    final check of webhook trigger and website hosting from EC2

--- app.py
+++ app.py
@@ -161,9 +161,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(host="0.0.0.0", port=8000, debug=True)
+app.run(host="0.0.0.0", port=8000)
 
-    #force change
-    #test
-    #final fix 
-    #updated version
